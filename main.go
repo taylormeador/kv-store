e507e74ef77f2a1a08191464c146789f7dd37041
Taylor <taylor.r.meador@gmail.com>
2025-10-23 16:18:43 -0600

    accept connections

--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net"
+)
+
+const PORT_NUMBER string = ":8080"
+
+func handleConnection(conn net.Conn) {
+
+	data := "hello world\r\n"
+	_, err := conn.Write([]byte(data))
+	if err != nil {
+		log.Println(err)
+	}
+}
 
 func main() {
-	fmt.Println("Hello world")
+	log.Printf("Starting server on localhost%s", PORT_NUMBER)
+
+	ln, err := net.Listen("tcp", PORT_NUMBER)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	for {
+		conn, err := ln.Accept()
+		log.Printf("Connected to %s", conn.RemoteAddr().String())
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		go handleConnection(conn)
+	}
 }
