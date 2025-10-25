820114f2111b35de222a7e6ef88d7b45c0aaa696
Taylor <taylor.r.meador@gmail.com>
2025-10-25 13:17:54 -0600

    only flush to disk every second in background, relax durability constraint

--- internal/wal/wal.go
+++ internal/wal/wal.go
@@ -3,34 +3,86 @@ package wal
 import (
 	"bufio"
 	"os"
+	"sync"
+	"time"
 
 	"github.com/taylormeador/kv-store/internal/protocol"
 	"github.com/taylormeador/kv-store/internal/store"
 )
 
 type WAL struct {
-	filepath string
-	file     *os.File
+	filepath   string
+	file       *os.File
+	mu         sync.Mutex
+	shutdownCh chan struct{}
+	wg         sync.WaitGroup
 }
 
 // Creates a WAL at the given filepath
 func NewWAL(filepath string) (*WAL, error) {
 	file, err := os.OpenFile(filepath, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
-	return &WAL{file: file, filepath: filepath}, err
+	if err != nil {
+		return nil, err
+	}
+
+	w := &WAL{
+		filepath:   filepath,
+		file:       file,
+		shutdownCh: make(chan struct{}),
+	}
+
+	// Start background fsync goroutine (every 1 second)
+	w.wg.Add(1)
+	go w.fsyncLoop()
+
+	return w, nil
 }
 
 // Close the file
 func (w *WAL) Close() error {
+	close(w.shutdownCh)
+
+	// Wait for final fsync
+	w.wg.Wait()
+
 	return w.file.Close()
 }
 
-// Write command to file and fsync
+// Write command to file
 func (w *WAL) Append(command protocol.Command) error {
+	w.mu.Lock()
+	defer w.mu.Unlock()
+
+	// Write to OS buffer, let fsyncLoop() handle flush
 	_, err := w.file.WriteString(command.String() + "\n")
 	if err != nil {
 		return err
 	}
-	return w.file.Sync()
+	return nil
+}
+
+// Background goroutine that fsyncs every second
+func (w *WAL) fsyncLoop() {
+	defer w.wg.Done()
+
+	ticker := time.NewTicker(1 * time.Second)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ticker.C:
+			w.mu.Lock()
+			w.file.Sync() // Force buffered data to disk
+			w.mu.Unlock()
+
+		case <-w.shutdownCh:
+			// Final fsync on shutdown
+			w.mu.Lock()
+			w.file.Sync()
+			w.mu.Unlock()
+			return
+		}
+	}
 }
 
 // Replays WAL and executes on store, restoring last known state
